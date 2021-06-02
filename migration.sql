351bb1948cafc64afb4c876554801b6e6d48f09c
Jake Chadwell <jake.chadwell01@gmail.com>
2021-06-02 12:51:45 -0500

    Added config to gitignore

---
+++ migration.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS adlister_db;
+CREATE DATABASE adlister_db;
+USE adlister_db;
+
+CREATE TABLE users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(150) UNIQUE NOT NULL,
+    email VARCHAR(200) UNIQUE NOT NULL,
+    password VARCHAR(150) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+    title VARCHAR(250) NOT NULL,
+    description TEXT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,64 @@
+import java.util.List;
+import java.sql.DriverManager;
+import java.sql.*;
+import java.util.ArrayList;
+import com.mysql.cj.jdbc.Driver;
+
+public class MySQLAdsDao implements Ads{
+    private Connection connection;
+
+    public MySQLAdsDao(Config config){
+        try{
+            DriverManager.registerDriver(new Driver());
+            this.connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        }catch(SQLException e){
+            throw new RuntimeException("Error connecting to SQL database.", e);
+        }
+    }
+
+    @Override
+    public List<Ad> all() {
+        List<Ad> ads = new ArrayList<>();
+        try{
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM ads");
+            while(rs.next()){
+                ads.add(new Ad(
+                        rs.getLong("id"),
+                        rs.getLong("user_id"),
+                        rs.getString("title"),
+                        rs.getString("description")
+                ));
+            }
+        }catch(SQLException e){
+            throw new RuntimeException("Error retrieving all ads.", e);
+        }
+        return ads;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        long success;
+        try{
+            Statement stmt = connection.createStatement();
+            String query = String.format("INSERT INTO ads(user_id, title, description) VALUES(%d, '%s', '%s')",
+                    ad.getUserId(),
+                    ad.getTitle(),
+                    ad.getDescription()
+                );
+            stmt.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            success = ad.getUserId();
+        }catch(SQLException e){
+            throw new RuntimeException("Error creating new ad", e);
+        }
+        return success;
+    }
+
+
+}
